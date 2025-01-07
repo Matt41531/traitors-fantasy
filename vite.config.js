f3530361b173bcfc93857c090cfe410b6cc0ed83
Matthew Rife <dxfan218@yahoo.com>
2025-01-06 22:40:13 -0500

    moving to host 0.0.0.0 for render

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -6,12 +6,23 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  console.log('VITE_PORT:', env.VITE_PORT);
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    server: {
+      host: '0.0.0.0',
+      port: parseInt(env.VITE_PORT) || 5173,
+    }
+  }
 })
